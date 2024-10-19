e6497f537da327ebaf2916691989076e05e2173a
tiartrop <90901624+tiartrop@users.noreply.github.com>
2024-10-20 04:45:58 +0800

    website: add dark theme

--- website/src/App.vue
+++ website/src/App.vue
@@ -7,13 +7,13 @@ import ReadMe from 'vue-paper-corners/README.md';
 const { description } = pkg;
 const link = 'https://github.com/tiartrop/vue-paper-corners';
 const graph =
-  'linear-gradient(rgb(233, 233, 233) 1px, transparent 0) 0 center / 20px 20px, linear-gradient(to right, rgb(233, 233, 233) 1px, #fff 0) 0 0 / 20px 20px;';
+  'linear-gradient(rgba(212,  212 , 212, .4) 1px, transparent 0) 0 center / 20px 20px, linear-gradient(to right, rgba(212,  212 , 212, .4) 1px, var(--text-bg-primary) 0) 0 0 / 20px 20px;';
 const dot =
-  'radial-gradient(rgb(233, 233, 233) 2px, transparent 0) 0 0 / 20px 20px, radial-gradient(rgb(233, 233, 233) 2px, #fff 2px)  0 0 / 20px 20px;';
+  'radial-gradient(rgba(212,  212 , 212, .15) 2px, transparent 0) 0 0 / 20px 20px, radial-gradient(rgba(212,  212 , 212, .15) 2px, var(--text-bg-primary) 2px)  0 0 / 20px 20px;';
 const paper = import.meta.env.DEV ? 'url(src/assets/paper-bg.png) repeat' : 'url(assets/paper-bg.png) repeat';
 
 const size = ref(30);
-const color = ref('#fff');
+const color = ref('var(--text-bg-primary)');
 const positionX = ref<'right' | 'left'>('right');
 const positionY = ref<'top' | 'bottom'>('bottom');
 </script>
@@ -25,7 +25,7 @@ const positionY = ref<'top' | 'bottom'>('bottom');
         :link="link"
         :size="size"
         :color="color"
-        bg-color="#e6e6e6"
+        bg-color="var(--background-primary)"
         :position-x="positionX"
         :position-y="positionY"
       >
@@ -48,7 +48,7 @@ const positionY = ref<'top' | 'bottom'>('bottom');
         <button @click="color = graph">Graph</button>
         <button @click="color = dot">Dot</button>
         <button @click="color = paper">Paper</button>
-        <button @click="color = '#fff'">White</button>
+        <button @click="color = 'var(--text-bg-primary)'">Default</button>
       </div>
     </header>
     <div class="markdown-body markdown">
@@ -59,6 +59,7 @@ const positionY = ref<'top' | 'bottom'>('bottom');
 
 <style scoped>
 header {
+  color: var(--text-primary);
   text-align: center;
   min-height: calc(100vh - 100px);
   padding-top: 47px;
@@ -76,6 +77,7 @@ header img {
 }
 
 header a {
+  color: inherit;
   font-weight: bold;
   line-height: 40px;
 }
@@ -100,11 +102,24 @@ header p {
   margin: 0 2px;
   font-size: 12px;
   font-weight: 700;
-  text-shadow: 0 1px hsla(0, 0%, 100%, 0.9);
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
   cursor: pointer;
   transition: all 0.3s;
-  background: #fff;
+  background: var(--text-bg-primary);
+}
+
+@media (prefers-color-scheme: light) {
+  .button button {
+    text-shadow: 0 1px hsla(0, 0%, 100%, 0.9);
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+  }
+}
+
+@media (prefers-color-scheme: dark) {
+  .button button {
+    background: rgba(78, 78, 78);
+    text-shadow: 0 1px hsla(0, 0%, 0%, 0.9);
+    box-shadow: 0 1px 3px rgba(255, 255, 255, 0.1);
+  }
 }
 
 .button button + button {
@@ -115,6 +130,7 @@ header p {
   outline: 0;
   box-shadow: 0 0 1px 2px rgba(255, 255, 255, 0.3);
 }
+
 .button button:focus {
   box-shadow: 0 0 1px 2px rgba(255, 255, 255, 0.3);
 }
@@ -125,6 +141,5 @@ header p {
   margin-bottom: 110px;
   padding: 20px 40px;
   border-radius: 5px;
-  background: #fff;
 }
 </style>
