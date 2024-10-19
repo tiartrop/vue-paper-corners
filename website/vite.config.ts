6460c1c6a3ef6b5445ae881ded629d514ed7890c
tiartrop <90901624+tiartrop@users.noreply.github.com>
2024-10-20 02:41:40 +0800

    chore: upgrade package dependencies

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import Markdown from 'vite-plugin-vue-markdown';
+import Markdown from 'unplugin-vue-markdown/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
       include: [/\.vue$/, /\.md$/],
     }),
     vueJsx(),
-    Markdown(),
+    Markdown({}),
   ],
   resolve: {
     alias: {
